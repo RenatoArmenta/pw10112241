142ce6ee19fc24b62aedeb4761f8d89d4b9e73cd
RenatoArmenta <127435729+RenatoArmenta@users.noreply.github.com>
2024-04-19 10:34:49 -0700

    Add files via upload

---
+++ pw10112241/JS/js/miscript.js
@@ -0,0 +1,10 @@
+var a=0;
+        let b=0; //scope - alcance - ambito
+        const pi = 3.141590;
+        alert("JS en Proceso")
+        function boton(){
+            alert("clic")
+        }
+        const flecha = ()=>{
+            alert("Funcion flecha - anonima")
+        }
