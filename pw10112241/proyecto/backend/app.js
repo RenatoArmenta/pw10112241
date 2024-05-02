3860d3c4d3698670dbbaf36b68940d90afcca888
RenatoArmenta <127435729+RenatoArmenta@users.noreply.github.com>
2024-05-02 10:57:29 -0700

    update sql

--- pw10112241/proyecto/backend/app.js
+++ pw10112241/proyecto/backend/app.js
@@ -37,7 +37,60 @@ app.get('/api/clientes',(req,res)=>{
         }
     });
 })
-
+//seleccionamos un cliente
+app.get('/api/clientes/:id',(req,res)=>{
+    conexion.query('SELECT * FROM clientes WHERE id=?',[req.params.id],(error,fila)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(fila);
+        }
+    })
+});
+//borrar un cliente
+app.delete('/api/clientes/:id',(req,res) =>{
+    let id = req.params.id;
+    conexion.query('DELETE FROM clientes WHERE id=?',[id],(error,filas)=>{
+        if(error){
+            throw error
+        }else{
+            res.send(filas)
+        }
+    })
+});
+//Insertar un nuevo cliente
+app.post('/api/clientes/:id',(req,res) =>{
+    let data = {
+        id:req.body.id,
+        nombre:req.body.nombre,
+        direccion:req.body.direccion,
+        telefono:req.body.telefono,
+        rfc:req.body.rfc
+    }
+    let sql = "INSERT INTO clientes SET ?";
+    conexion.query(sql,data,(error,resultado)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(resultado);
+        }
+    })
+});
+app.put('/api/clientes/:id',(req,res) => {
+    let id = req.params.id;
+    let nombre=req.body.nombre;
+    let direccion=req.body.direccion;
+    let telefono=req.body.telefono;
+    let rfc=req.body.rfc;
+    let sql="UPDATE clientes SET nombre=?, direccion=?, telefono=?, rfc=? WHERE id=?";
+    conexion.query(sql,[nombre,direccion,telefono,rfc,id],(error,results)=> {
+        if(error){
+            throw error;
+        }else{
+            res.send()
+        }
+    })
+});
 //Encender el servidor
 let puerto = 3000;
 app.listen(puerto,function(){
