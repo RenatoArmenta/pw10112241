1b375079f2856bdec0f9ba0a7122b422effb22f5
RenatoArmenta <127435729+RenatoArmenta@users.noreply.github.com>
2024-05-14 11:02:00 -0700

    Eliminar y Agregar Clientes

---
+++ pw10112241/frontend/tienda/src/views/ClientesCreateView.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-header">
+                <h4>Agregar cliente</h4>
+            </div>
+            <div class="card-body">
+                <div class="mb-3">
+                    Id
+                    <input type="text" class="form-control" v-model="model.cliente.id">
+                </div>
+                <div class="mb-3">
+                    Nombre
+                    <input type="text" class="form-control" v-model="model.cliente.nombre">
+                </div>
+                <div class="mb-3">
+                    Direccion
+                    <input type="text" class="form-control" v-model="model.cliente.direccion">
+                </div>
+                <div class="mb-3">
+                    Telefono
+                    <input type="text" class="form-control" v-model="model.cliente.telefono">
+                </div>
+                <div class="mb-3">
+                    RFC
+                    <input type="text" class="form-control" v-model="model.cliente.rfc">
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-primary">Guardar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "ClientesCreate",
+    data(){
+        return{
+            model:{
+                cliente:{
+                    id: '',
+                    nombre: '',
+                    direccion: '',
+                    telefono: '',
+                    rfc: ''
+                }
+            }
+        }
+    }
+}
+</script>
