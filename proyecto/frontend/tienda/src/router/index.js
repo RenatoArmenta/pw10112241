2f0549ecfca0612653bb2042fd347182fc3bc464
RenatoArmenta <127435729+RenatoArmenta@users.noreply.github.com>
2024-05-16 10:56:59 -0700

    guardar terminado

---
+++ proyecto/frontend/tienda/src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import ClientesView from '../views/ClientesView.vue'
+import ClientesCreateView from '../views/ClientesCreateView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/clientes',
+      name: 'clientes',
+      component: ClientesView
+    },
+    {
+      path: '/clientes/create',
+      name: 'clientescreate',
+      component: ClientesCreateView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
